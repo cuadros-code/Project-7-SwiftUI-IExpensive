66a492be2e6e6803f3b2c1741a0fdfc97776d334
kevin cuadros <kevindavidcuadros@gmail.com>
2024-11-04 11:27:10 -0500

    Initial Commit

---
+++ Project-7-SwiftUI-IExpensive/Project_7_SwiftUI_IExpensiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_7_SwiftUI_IExpensiveApp.swift
+//  Project-7-SwiftUI-IExpensive
+//
+//  Created by Kevin Cuadros on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_7_SwiftUI_IExpensiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
