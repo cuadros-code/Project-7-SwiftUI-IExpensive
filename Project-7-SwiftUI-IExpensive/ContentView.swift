a349b9bd8bfe43069282f933d5c6908b8a4f7a26
kevin cuadros <kevindavidcuadros@gmail.com>
2024-11-04 16:12:18 -0500

    on delete

--- Project-7-SwiftUI-IExpensive/ContentView.swift
+++ Project-7-SwiftUI-IExpensive/ContentView.swift
@@ -6,47 +6,38 @@
 //
 import SwiftUI
 
-@Observable
-class User {
-    var name = "Kevin"
-    var lastName = "Cuadros"
-}
-
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    let name: String
-    
-    var body: some View {
-        Text("Hello, \(name)!")
-        Button("Dismiss"){
-            dismiss()
-        }
-    }
-}
 
 struct ContentView: View {
     
-    @State private var user = User()
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-    
-        VStack {
-            Text("Your name is \(user.name) \(user.lastName)")
-            
-            TextField("First name", text: $user.name)
-            TextField("Last name", text: $user.lastName)
-            
-            Button("Show Sheet") {
-                showingSheet.toggle()
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self)  {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+                
             }
-            
-            .sheet(isPresented: $showingSheet) {
-                SecondView(name: user.name)
+            .toolbar {
+                EditButton()
             }
         }
-        .padding()
     }
+    
+    func removeRows(at offsets: IndexSet){
+        numbers.remove(atOffsets: offsets)
+    }
+    
 }
 
 #Preview {
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 #  IExpensive
 
+## Sharing SwiftUI state with @Observable
+
 ```swift
 import SwiftUI
 
@@ -49,3 +51,49 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+```
+
+
+## Deleting items using onDelete()
+
+```swift 
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self)  {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+                
+            }
+            .toolbar {
+                EditButton()
+            }
+        }
+    }
+    
+    func removeRows(at offsets: IndexSet){
+        numbers.remove(atOffsets: offsets)
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
+```
