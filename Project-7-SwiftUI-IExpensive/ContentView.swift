ed133576337aad4826a1fb6a89ad3dbd64bcaa3b
kevin cuadros <kevindavidcuadros@gmail.com>
2024-11-05 20:30:26 -0500

    add total

--- Project-7-SwiftUI-IExpensive/ContentView.swift
+++ Project-7-SwiftUI-IExpensive/ContentView.swift
@@ -19,8 +19,8 @@ import SwiftUI
 //    Text(item.name)
 // }
 
-struct ExpenseItem: Identifiable {
-    let id = UUID()
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let type: String
     let amount: Double
@@ -28,31 +28,81 @@ struct ExpenseItem: Identifiable {
 
 @Observable
 class Expenses {
-    var items = [ExpenseItem]()
+    
+    var total = 0.0
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+            total = 0.0
+            items.forEach { item in
+                total += item.amount
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodeItems = try? JSONDecoder().decode(
+                [ExpenseItem].self,
+                from: savedItems
+            ) {
+                items = decodeItems
+                return
+            }
+        }
+        
+        items = []
+    }
+    
 }
 
 
 struct ContentView: View {
     
     @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationStack {
          
             List {
-                ForEach(expenses.items) { item in
-                    Text(item.name)
+                Section {
+                    ForEach(expenses.items) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                                    .font(.subheadline)
+                            }
+                            Spacer()
+                            Text(item.amount, format: .currency(code: "COP"))
+                        }
+                    }
+                    .onDelete(perform: removeItem)
                 }
-                .onDelete(perform: removeItem)
+                
+                Section {
+                    HStack {
+                        Text("Total")
+                            .font(.headline)
+                        Spacer()
+                        Text(expenses.total, format: .currency(code: "COP"))
+                    }
+                }
+            }
+            
+            
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
             
             .navigationTitle("iExpenses")
             .toolbar {
                 Button("Add Expenses", systemImage: "plus") {
-                    let expense = ExpenseItem(name: "Test", type: "Persnal", amount: 12.00)
-                    withAnimation {
-                     expenses.items.append(expense)
-                    }
+                    showingAddExpense.toggle()
                 }
             }
         }
